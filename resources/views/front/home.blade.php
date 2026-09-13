dc5d153e6fc625afc55362a41fbf24af75b17d81
mutasem <mutasemms198@gmail.com>
2026-09-13 21:51:03 +0300

    first commit

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -279,7 +279,6 @@
 @endif
 
 <div class="wrap"><div class="grout"></div></div>
-
 <!-- CONTACT -->
 <section class="section" id="contact">
   <div class="wrap">
@@ -288,7 +287,7 @@
       <h2>{{ __('messages.contact_title') }}</h2>
       <p>{{ __('messages.contact_desc') }}</p>
     </div>
-    <div class="contact-grid">
+    <div dir="{{ app()->getLocale() === 'ar' ? 'rtl' : 'ltr' }}" class="contact-grid">
       <div class="contact-info reveal">
         <div class="cinfo"><div class="ci"><svg viewBox="0 0 24 24" fill="none" stroke-width="1.8"><path d="M22 16.9v3a2 2 0 01-2.2 2 19.8 19.8 0 01-8.6-3 19.5 19.5 0 01-6-6 19.8 19.8 0 01-3-8.6A2 2 0 014.1 2h3a2 2 0 012 1.7c.1.9.3 1.8.7 2.7a2 2 0 01-.5 2.1L8.1 9.9a16 16 0 006 6l1.4-1.2a2 2 0 012.1-.5c.9.4 1.8.6 2.7.7a2 2 0 011.7 2z"/></svg></div><div><b>{{ __('messages.contact_label_phone') }}</b><span>{{ $contactInfo->phone }}</span></div></div>
         <div class="cinfo"><div class="ci"><svg viewBox="0 0 24 24" fill="none" stroke-width="1.8"><path d="M4 4h16v16H4z"/><path d="M4 6l8 6 8-6"/></svg></div><div><b>{{ __('messages.contact_label_email') }}</b><span>{{ $contactInfo->email }}</span></div></div>
